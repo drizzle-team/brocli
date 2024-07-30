107a3f62dc8609c29594d960c5f1c1383bf619d7
Sukairo-02 <sreka9056@gmail.com>
2024-07-30 13:26:41 +0300

    [WIP] Reworked default theme

--- src/event-handler.ts
+++ src/event-handler.ts
@@ -1,5 +1,5 @@
 import { type Command, command, getCommandNameWithParents } from './command-core';
-import type { ProcessedBuilderConfig } from './option-builder';
+import type { BuilderConfig, ProcessedBuilderConfig } from './option-builder';
 
 export type CommandHelpEvent = {
 	type: 'commandHelp';
@@ -87,6 +87,58 @@ export type BroCliEvent =
 
 export type BroCliEventType = BroCliEvent['type'];
 
+const getOptionTypeText = (option: BuilderConfig) => {
+	let result = '';
+
+	switch (option.type) {
+		case 'boolean':
+			result = '';
+			break;
+		case 'number': {
+			if ((option.minVal ?? option.maxVal) !== undefined) {
+				let text = '';
+
+				if (option.isInt) text = text + `integer `;
+
+				if (option.minVal !== undefined) text = text + `[${option.minVal};`;
+				else text = text + `(∞;`;
+
+				if (option.maxVal !== undefined) text = text + `${option.maxVal}]`;
+				else text = text + `∞)`;
+
+				result = text;
+				break;
+			}
+
+			if (option.isInt) {
+				result = 'integer';
+				break;
+			}
+
+			result = 'number';
+			break;
+		}
+		case 'string': {
+			if (option.enumVals) {
+				result = '[ ' + option.enumVals.join(' | ') + ' ]';
+				break;
+			}
+
+			result = 'string';
+			break;
+		}
+		case 'positional': {
+			result = `${option.isRequired ? '<' : '['}${option.enumVals ? option.enumVals.join('|') : option.name}${
+				option.isRequired ? '>' : ']'
+			}`;
+			break;
+		}
+	}
+
+	if (option.isRequired && option.type !== 'positional') result = '!' + result.length ? ' ' : '' + result;
+	return result;
+};
+
 /**
  * Return `true` if your handler processes the event
  *
@@ -96,46 +148,148 @@ export type EventHandler = (event: BroCliEvent) => boolean | Promise<boolean>;
 export const defaultEventHandler: EventHandler = async (event) => {
 	switch (event.type) {
 		case 'commandHelp': {
-			const options = event.command.options
-				? Object.values(event.command.options).filter((opt) => !opt.config?.isHidden).map(
-					({ config: opt }) => ({
-						name: opt.name,
-						aliases: opt.aliases.length ? `${opt.aliases.join(', ')}` : '-',
-						description: opt.description ?? '-',
-						type: opt.type,
-						required: opt.isRequired ? '✓' : '✗',
-					}),
-				)
-				: undefined;
+			const command = event.command;
+			const commandName = getCommandNameWithParents(command);
+			const cliName = event.cliName;
+			const desc = command.desc ?? command.shortDesc;
 
-			console.log(
-				`Command: ${event.command.name}${event.command.aliases ? ` [${event.command.aliases.join(', ')}]` : ''}${
-					event.command.desc ? ` - ${event.command.desc}` : ''
-				}`,
+			if (desc !== undefined) {
+				console.log(`\n${desc}`);
+			}
+
+			const opts = Object.values(command.options ?? {} as Exclude<typeof command.options, undefined>).filter((opt) =>
+				!opt.config.isHidden
 			);
+			const positionals = opts.filter((opt) => opt.config.type === 'positional');
+			const options = opts.filter((opt) => opt.config.type !== 'positional');
 
-			if (!options?.length) return true;
+			console.log('\nUsage:');
+			if (command.handler) {
+				console.log(
+					`  ${cliName ? cliName + ' ' : ''}${commandName}${
+						positionals.length
+							? ' '
+								+ positionals.map(({ config: p }) => getOptionTypeText(p)).join(' ')
+							: ''
+					} [flags]`,
+				);
+			} else console.log(`  ${cliName ? cliName + ' ' : ''}${commandName} [command]`);
 
-			console.log('\nOptions:');
-			console.table(options);
+			if (command.aliases) {
+				console.log(`\nAliases:`);
+				console.log(`  ${command.aliases.join(', ')}`);
+			}
+
+			if (command.subcommands) {
+				console.log('\nAvailable Commands:');
+				const padding = 3;
+				const maxLength = command.subcommands.reduce((p, e) => e.name.length > p ? e.name.length : p, 0);
+				const paddedLength = maxLength + padding;
+				const preDescPad = 2 + paddedLength;
+
+				const data = command.subcommands.map((s) =>
+					`  ${s.name.padEnd(paddedLength)}${
+						(() => {
+							const description = s.shortDesc ?? s.desc;
+							if (!description?.length) return '';
+							const split = description.split('\n');
+							const first = split.shift()!;
+
+							const final = [first, ...split.map((s) => ''.padEnd(preDescPad) + s)].join('\n');
+
+							return final;
+						})()
+					}`
+				)
+					.join('\n');
+				console.log(data);
+			}
+
+			if (options.length) {
+				const aliasLength = options.reduce((p, e) => {
+					const currentLength = e.config.aliases.reduce((pa, a) => pa + a.length, 0)
+						+ ((e.config.aliases.length - 1) * 2) + 1; // Names + coupling symbols ", " + ending coma
+
+					return currentLength > p ? currentLength : p;
+				}, 0);
+				const paddedAliasLength = aliasLength > 0 ? aliasLength + 1 : 0;
+				const nameLength = options.reduce((p, e) => {
+					const typeLen = getOptionTypeText(e.config).length;
+					const length = typeLen > 0 ? e.config.name.length + 1 + typeLen : e.config.name.length;
+
+					return length > p ? length : p;
+				}, 0) + 3;
+
+				const preDescPad = paddedAliasLength + nameLength + 2;
+
+				const data = options.map(({ config: opt }) =>
+					`  ${`${opt.aliases.length ? opt.aliases.join(', ') + ',' : ''}`.padEnd(paddedAliasLength)}${
+						`${opt.name}${
+							(() => {
+								const typeText = getOptionTypeText(opt);
+								return typeText.length ? ' ' + typeText : '';
+							})()
+						}`.padEnd(nameLength)
+					}${
+						(() => {
+							if (!opt.description?.length) {
+								return opt.default !== undefined
+									? `default: ${JSON.stringify(opt.default)}`
+									: '';
+							}
+
+							const split = opt.description.split('\n');
+							const first = split.shift()!;
+							const def = opt.default !== undefined ? ` (default: ${JSON.stringify(opt.default)})` : '';
+
+							const final = [first, ...split.map((s) => ''.padEnd(preDescPad) + s)].join('\n') + def;
+
+							return final;
+						})()
+					}`
+				).join('\n');
+
+				console.log('\nFlags:');
+				console.log(data);
+			}
+
+			console.log('\nGlobal flags:');
+			console.log(`  -h, --help      help for ${commandName}`);
+			console.log(`  -v, --version   version${cliName ? ` for ${cliName}` : ''}`);
+
+			if (command.subcommands?.length) {
+				console.log(
+					`\nUse "${
+						cliName ? cliName + ' ' : ''
+					}${commandName} [command] --help" for more information about a command.\n`,
+				);
+			}
 
 			return true;
 		}
 
 		case 'globalHelp': {
-			const cmds = event.commands.filter((cmd) => !cmd.hidden);
+			const cliName = event.cliName;
+			console.log('Usage:');
+			console.log(`  ${cliName ? cliName + ' ' : ''}[command]`);
 
-			const tableCmds = cmds.map((cmd) => ({
-				name: cmd.name,
-				aliases: cmd.aliases ? cmd.aliases.join(', ') : '-',
-				description: cmd.desc ?? '-',
-			}));
+			if (event.commands) {
+				console.log('\nAvailable Commands:');
+				const padding = 3;
+				const maxLength = event.commands.reduce((p, e) => e.name.length > p ? e.name.length : p, 0);
+				const paddedLength = maxLength + padding;
 
-			console.log(`Here's the list of all available commands:`);
-			console.table(tableCmds);
-			console.log(
-				'To read the details about any particular command type: [commandName] --help',
-			);
+				const data = event.commands.map((s) =>
+					`  ${s.name.padEnd(paddedLength)}${(s.shortDesc ?? s.desc)?.split('\n').shift()!}`
+				)
+					.join('\n');
+				console.log(data);
+			}
+
+			console.log('\nFlags:');
+			console.log(`  -h, --help      help${cliName ? ` for ${cliName}` : ''}`);
+			console.log(`  -v, --version   version${cliName ? ` for ${cliName}` : ''}`);
+			console.log('\n');
 
 			return true;
 		}
